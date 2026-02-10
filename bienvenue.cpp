c7ddf45b56464a29707f2625d73c5416489ce4b4
yosra <yosraboubaker61@gmail.com>
2026-02-10 09:46:31 +0100

    Mise à jour du programme pour afficher 'Bienvenue le monde \!'

--- bienvenue.cpp
+++ bienvenue.cpp
@@ -1,7 +1,8 @@
-// TODO Indiquer ce que fait le programme
+// Affiche un message de bienvenue
+#include <iostream>
 int main()
 {
-    // TODO Afficher un message de bienvenue
+    std::cout << "Bienvenue le monde !" << std::endl;
     return 0;
 }
 
